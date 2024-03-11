c03031c6a97dc3aa22a516249291f7b1584f4c57
pili-zhangqiu <p.zhangqiu@gmail.com>
2024-03-11 21:18:24 +0000

    feat(plot): Generate scatter and box plots
    Used to explore and understand data

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from data_processor import DataProcessor
+from data_plotter import DataPlotter
 
 if __name__ == '__main__':
-    data = DataProcessor('sites_data.csv')
-    #print(data.df_training_clean)
+    # Setup - Load and clean data
+    data_processor = DataProcessor('sites_data.csv')
+    data = data_processor.df_training_clean
+
+    # Explore the data - Plots
+    plotter = DataPlotter(data)
+    
+    plotter.scatterplot_headcount_vs_sales_per_site()
+    plotter.scatterplot_headcount_vs_sales_per_site_period("morning")
+    plotter.scatterplot_headcount_vs_sales_per_site_period("afternoon")
+    plotter.scatterplot_headcount_vs_sales_per_site_period("evening")
+    
+    plotter.boxplot_headcount_per_site_period()
+    plotter.boxplot_sales_per_site_period_a()
+    plotter.boxplot_sales_per_site_period_b()
