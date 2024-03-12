4949b71fe92ce94b32658b9cb6f9c2f6cd548dbd
pili-zhangqiu <p.zhangqiu@gmail.com>
2024-03-12 08:29:47 +0000

    feat(table): Describe dataframe columns
    - Sales
    - Total profits
    - Avg profits per headcount

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from data_plotter import DataPlotter
 if __name__ == '__main__':
     # Setup - Load and clean data
     data_processor = DataProcessor('sites_data.csv')
-    data = data_processor.df_training_clean
+    data = data_processor.df_training_extended
 
     # Explore the data - Plots
+    # Describe
+    data_processor.describe_df_column("sales")
+    data_processor.describe_df_column("total_profit")
+    data_processor.describe_df_column("avg_profit_per_headcount")
+
+    # Plot
     plotter = DataPlotter(data)
     plotter.violinplot_per_site_period(x="site", y="avg_profit_per_headcount", hue="period_of_day")
     
